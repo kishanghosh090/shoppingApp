3a3e6f484ff6e0717a9973697598f1f78137dc86
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-19 00:10:12 +0530

    add cards

--- shoppingApp/HomeScreenViews/DetailsView.swift
+++ shoppingApp/HomeScreenViews/DetailsView.swift
@@ -18,6 +18,34 @@ struct RoundenedCornerShape: Shape{
     }
 }
 
+struct Counter: View {
+    @State var count = 0
+    
+    var body: some View{
+        HStack(spacing: 25){
+            Button{
+                count += 1
+            }label: {
+                Image(systemName: "plus")
+            }
+            Text("\(count)")
+                .font(.title)
+            Button{
+                if count != 0 {count += 1}
+            }label: {
+                Image(systemName: "minus")
+            }
+            
+        }
+        .foregroundStyle(.black)
+        .frame(width: 100, height: 40)
+        .padding()
+        .background(Color("bgColor").opacity(0.15))
+        .cornerRadius(12)
+        
+    }
+}
+
 struct DetailsView: View {
     @State var fruit: fruitTitle
     var body: some View {
@@ -62,7 +90,24 @@ struct DetailsView: View {
                     Text("25-30 Min")
                 }
             }.padding(.horizontal)
-            
+            HStack{
+                Text("$1.99")
+                    .fontWeight(.medium)
+                    .font(.title)
+                    .padding(.horizontal)
+                Spacer()
+                Counter()
+            }
+            Image("bg")
+                .resizable()
+                .frame(width: 350, height: 50, alignment: .center)
+                .cornerRadius(15)
+                .shadow(color: .gray, radius: 5, x: 5, y: 5)
+                .overlay{
+                    Text("Add to cart")
+                        .font(.system(.title3))
+                        .fontWeight(.medium)
+                }.padding(.horizontal)
         }
     }
 }
